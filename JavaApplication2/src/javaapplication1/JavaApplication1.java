17b0ac65ad5a06339558a95633e9d0a176baa7aa
Saad Sharada <84268450+saadsharad@users.noreply.github.com>
2021-06-05 17:14:30 +0200

    Add files via upload

---
+++ JavaApplication2/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author saad mustafa
+ */
+public class JavaApplication1 {
+
+   
+    public static void main(String[] args) {
+     
+        NewJFrame F1 = new NewJFrame();
+        F1.setLocationRelativeTo(null);    
+        F1.setVisible(true);
+      //  F1.getContentPane().setBackground(new color (255,255,255));
+    
+               }
+    
+}
---
+++ JavaApplication2/src/javaapplication1/myclass.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author saad mustafa
+ */
+class myclass {
+    
+    String Name,Date,Address,Mobileno,Accountno,Accounttype,acc,v; 
+int balance,withdrawal0;
+Integer value=0,Accountno_index, Enter_the_accountno_num1,Enter_the_accountno_num2, balance6, balance8;
+String Enter_the_accountno_text1,Enter_the_accountno_text2,The_balance_to_be_sent_text;
+int Final_balance_after_transfer_1,Final_balance_after_transfer_2,The_balance_to_be_sent_num;
+String data[][]= new String [1000][9];
+String price="0";
+  int Account_number ;
+    
+}
